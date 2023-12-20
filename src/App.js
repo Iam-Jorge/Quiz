ef8adb79c6010e03071e951c364d4d26b08fb555
Iam-Jorge <Angelovg7498@gmail.com>
2023-12-20 13:27:22 +0100

    Choose exam and images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import examsData from './questions.json'; // Asegúrate de que la ruta sea correcta
+import examsData from './questions.json';
 
 function App() {
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
   const [exams, setExams] = useState([]);
   const [selectedExam, setSelectedExam] = useState(null);
 
@@ -18,9 +19,20 @@ function App() {
     setScore(0);
     setCurrentQuestion(0);
     setShowResults(false);
+    setUserAnswers([]);
   };
 
-  const optionClicked = (isCorrect) => {
+  const repeatExam = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+    setUserAnswers([]);
+  };
+
+  const optionClicked = (isCorrect, optionId) => {
+    const newUserAnswers = [...userAnswers, { questionId: currentQuestion, selectedOptionId: optionId }];
+    setUserAnswers(newUserAnswers);
+
     if (isCorrect) {
       setScore(score + 1);
     }
@@ -33,36 +45,60 @@ function App() {
     }
   };
 
+  const chooseAnotherExam = () => {
+    setSelectedExam(null);
+  };
+
+  const renderQuestionCard = () => {
+    const question = selectedExam.questions[currentQuestion];
+    return (
+      <div className="question-card">
+        <h2>Pregunta: {currentQuestion + 1} / {selectedExam.questions.length}</h2>
+        <h3 className="question-text">{question.text}</h3>
+        {question.questionURL && <img src={question.questionURL} alt="Question" />}
+        <ul>
+          {question.options.map((option) => (
+            <li key={option.id} onClick={() => optionClicked(option.isCorrect, option.id)}>
+              {option.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const renderResults = () => {
+    return (
+      <div className="final-results">
+        <h1>Resultado</h1>
+        <h2>{score} de {selectedExam.questions.length} correctas - {(score / selectedExam.questions.length) * 100}%</h2>
+        {userAnswers.map((answer) => {
+          const question = selectedExam.questions[answer.questionId];
+          const selectedOption = question.options.find(option => option.id === answer.selectedOptionId);
+          const correctOption = question.options.find(option => option.isCorrect);
+
+          if (!selectedOption.isCorrect) {
+            return (
+              <div key={answer.questionId}>
+                <h4>{question.text}</h4>
+                <p>Tu respuesta: {selectedOption.text}</p>
+                <p>Respuesta correcta: {correctOption.text}</p>
+              </div>
+            );
+          }
+          return null;
+        })}
+        <button onClick={repeatExam}>Repetir examen</button>
+        <button onClick={chooseAnotherExam}>Elegir otro examen</button>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <h1>USA Quiz 🇺🇸</h1>
+      <h1>Sistemas Distribuidos</h1>
       {selectedExam ? (
-        showResults ? (
-          <div className="final-results">
-            <h1>Final Results</h1>
-            <h2>
-              {score} out of {selectedExam.questions.length} correct - (
-              {(score / selectedExam.questions.length) * 100}%)
-            </h2>
-            <button onClick={() => setSelectedExam(null)}>Choose another exam</button>
-          </div>
-        ) : (
-          <div className="question-card">
-            <h2>
-              Question: {currentQuestion + 1} out of {selectedExam.questions.length}
-            </h2>
-            <h3 className="question-text">
-              {selectedExam.questions[currentQuestion].text}
-            </h3>
-            <ul>
-              {selectedExam.questions[currentQuestion].options.map((option) => (
-                <li key={option.id} onClick={() => optionClicked(option.isCorrect)}>
-                  {option.text}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )
+        showResults ? renderResults() : renderQuestionCard()
       ) : (
         exams.map((exam, index) => (
           <button key={index} onClick={() => startExam(exam)}>
