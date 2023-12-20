5754467954c33d8b905716af58939b15bff085aa
Iam-Jorge <Angelovg7498@gmail.com>
2023-12-20 13:01:38 +0100

    Add exams

--- src/App.js
+++ src/App.js
@@ -1,85 +1,74 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import questionsData from './questions.json';
+import examsData from './questions.json'; // Asegúrate de que la ruta sea correcta
 
 function App() {
-  // Properties
   const [showResults, setShowResults] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
-  const [questions, setQuestions] = useState([]);
+  const [exams, setExams] = useState([]);
+  const [selectedExam, setSelectedExam] = useState(null);
 
-  // Cargar preguntas cuando el componente se monta
   useEffect(() => {
-    // Aquí, simplemente establecemos las preguntas desde el archivo JSON
-    setQuestions(questionsData);
+    setExams(examsData);
   }, []);
 
-  // Helper Functions
-
-  /* A possible answer was clicked */
-  const optionClicked = (isCorrect) => {
-    // Increment the score
-    if (isCorrect) {
-      setScore(score + 1);
-    }
-
-    if (currentQuestion + 1 < questions.length) {
-      setCurrentQuestion(currentQuestion + 1);
-    } else {
-      setShowResults(true);
-    }
-  };
-
-  /* Resets the game back to default */
-  const restartGame = () => {
+  const startExam = (exam) => {
+    setSelectedExam(exam);
     setScore(0);
     setCurrentQuestion(0);
     setShowResults(false);
   };
 
+  const optionClicked = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < selectedExam.questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowResults(true);
+    }
+  };
+
   return (
     <div className="App">
-      {/* 1. Header  */}
       <h1>USA Quiz 🇺🇸</h1>
-
-      {/* 2. Current Score  */}
-      <h2>Score: {score}</h2>
-
-      {/* 3. Show results or show the question game  */}
-      {showResults ? (
-        /* 4. Final Results */
-        <div className="final-results">
-          <h1>Final Results</h1>
-          <h2>
-            {score} out of {questions.length} correct - (
-            {(score / questions.length) * 100}%)
-          </h2>
-          <button onClick={() => restartGame()}>Restart game</button>
-        </div>
-      ) : (
-        /* 5. Question Card  */
-        <div className="question-card">
-          {/* Current Question  */}
-          <h2>
-            Question: {currentQuestion + 1} out of {questions.length}
-          </h2>
-          <h3 className="question-text">{questions[currentQuestion].text}</h3>
-
-          {/* List of possible answers  */}
-          <ul>
-            {questions[currentQuestion].options.map((option) => {
-              return (
-                <li
-                  key={option.id}
-                  onClick={() => optionClicked(option.isCorrect)}
-                >
+      {selectedExam ? (
+        showResults ? (
+          <div className="final-results">
+            <h1>Final Results</h1>
+            <h2>
+              {score} out of {selectedExam.questions.length} correct - (
+              {(score / selectedExam.questions.length) * 100}%)
+            </h2>
+            <button onClick={() => setSelectedExam(null)}>Choose another exam</button>
+          </div>
+        ) : (
+          <div className="question-card">
+            <h2>
+              Question: {currentQuestion + 1} out of {selectedExam.questions.length}
+            </h2>
+            <h3 className="question-text">
+              {selectedExam.questions[currentQuestion].text}
+            </h3>
+            <ul>
+              {selectedExam.questions[currentQuestion].options.map((option) => (
+                <li key={option.id} onClick={() => optionClicked(option.isCorrect)}>
                   {option.text}
                 </li>
-              );
-            })}
-          </ul>
-        </div>
+              ))}
+            </ul>
+          </div>
+        )
+      ) : (
+        exams.map((exam, index) => (
+          <button key={index} onClick={() => startExam(exam)}>
+            {exam.examName}
+          </button>
+        ))
       )}
     </div>
   );
